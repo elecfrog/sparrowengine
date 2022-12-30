425b090b3b7a3eb6b65d66e58190067fbb423f1c
elecfrog <elecfrogdu@gmail.com>
2022-12-30 22:43:55 +0000

    [tmp push]

--- src/Application.cpp
+++ src/Application.cpp
@@ -17,6 +17,53 @@
 #include <imgui/backends/imgui_impl_opengl3.h>
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+struct ShaderProgramSourceTMP
+{
+	std::string vertexSource;
+	std::string fragmentSource;
+};
+
+
+static ShaderProgramSourceTMP ParseShaderSource(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType
+	{
+		NONE	 = -1,
+		VERTEX	 =  0,
+		FRAGMENT =  1
+	};
+	std::string line;
+
+	std::stringstream ss[2];
+
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line))
+	{
+		if (line.find("shader") != std::string::npos)
+		{
+			if (line.find("vertex") != std::string::npos)
+				type = ShaderType::VERTEX;
+				// set mode to vertex
+			else if (line.find("fragment") != std::string::npos)
+				// set mode to fragment
+				type = ShaderType::FRAGMENT;
+
+		}
+		else
+		{
+			ss[(int)type] << line << "\n";
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -83,8 +130,8 @@ int main(void)
 
 	glfwSetErrorCallback([](int error, const char *description) { std::cerr << "Error: " << description << std::endl; });
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
@@ -149,45 +196,38 @@ int main(void)
 		testMenu->RegisterTest<test::BatchingTextures>("Batching Textures");
 		testMenu->RegisterTest<test::BatchingTexturesDynamic>("Batching Textures (dynamic)");
 
-		float positions[6] = {
+		float positions[] = {
 				-0.5f, -0.5f,
-				0.f, 0.5f,
-				0.5f, -0.5f
+				0.5f, -0.5f,
+				0.5f, 0.5f,
+				-0.5f, 0.5f
 		};
 
-		unsigned int VAO;
+		unsigned int indices[] = {
+			0,1,2,
+			2,3,0,
+		};
+
+		unsigned int buffer, VAO;
 		glGenVertexArrays(1, &VAO);
-		unsigned int buffer;
 		glGenBuffers(1, &buffer);
 
 		glBindVertexArray(VAO);
-
+		
 		glBindBuffer(GL_ARRAY_BUFFER, buffer);
-		glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STREAM_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STREAM_DRAW);
 		
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+		unsigned int ibo;
+		glGenBuffers(1, &ibo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
-		std::string vertexShader =
-			"#version 330 core\n"
-			"\n"
-			"layout(location = 0) in vec4 position;\n"
-			"void main()\n"
-			"{\n"
-			"	gl_Position = position;\n"
-			"}\n";
+		ShaderProgramSourceTMP source = ParseShaderSource("res/shaders/Triangle.shader");
 
-		std::string fragmentShader = 
-			"#version 330 core\n"
-			"\n"
-			"layout(location = 0) out vec4 color;\n"
-			"void main()\n"
-			"{\n"
-			"	color = vec4(1.0,0.0,0.0,1.0);\n"
-			"}\n";
-
-		unsigned int shader = CreateShader(vertexShader, fragmentShader);
+		unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
 
 		glUseProgram(shader);
 
@@ -202,8 +242,8 @@ int main(void)
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			glBindVertexArray(VAO);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-
+			//glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 			// legacy OpenGL, just for debugging
 			//glBegin(GL_TRIANGLES);
@@ -258,6 +298,8 @@ int main(void)
 			glfwPollEvents();
 
 		} // while (!glfwWindowShouldClose(window))
+
+		glDeleteShader(shader);
 
 		//delete currentTest;
 		//if (currentTest != testMenu)
